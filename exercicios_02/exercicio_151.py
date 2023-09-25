2944a3a24c86c2f7998d1a2e778ad20063763fca
Rennerson13 <143669686+Rennerson13@users.noreply.github.com>
2023-09-25 13:34:23 -0300

    Update exercicio_151.py

--- exercicios_02/exercicio_151.py
+++ exercicios_02/exercicio_151.py
@@ -8,9 +8,9 @@ print(f'Imc de {imc}')
 if imc < 20:
     print(f'{nome} está abaixo do peso')
 elif imc >=20 and imc <= 25:
-    print(f'{nome} está com peso normal do peso')
+    print(f'{nome} está com peso normal')
 elif imc >25 and imc <= 30:
-    print(f'{nome} está com peso excesso do peso')
+    print(f'{nome} está com excesso do peso')
 elif imc >30 and imc <= 35:
     print(f'{nome} está com obesidade')
 else:
